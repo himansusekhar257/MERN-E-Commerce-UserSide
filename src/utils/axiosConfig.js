6d0f1b0ef6a8c28cbb17ace07f320e2979394287
himansusekhar257 <himansusekhar257@gmail.com>
2024-06-20 22:20:19 +0530

    new Ip

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 // export const base_url = 'http://localhost:5000/api/'
 
-export const base_url = 'http://15.206.116.144:5000/api/'
+export const base_url = 'http://3.111.218.61:5000/api/'
 // export const base_url = process.env.base_url
 console.log("baseUrl",base_url);
 const getTokenFromLocalStorage = localStorage.getItem("customer")
